30d39126f828a5438213620bf65bd6e82a0ab375
leadblade <pvl_git@i.ua>
2014-06-30 09:50:42 +0300

    test

--- gameoflife-acceptance-tests/src/test/java/com/wakaleo/gameoflife/webtests/WhenTheUserEntersAnInitialGrid.java
+++ gameoflife-acceptance-tests/src/test/java/com/wakaleo/gameoflife/webtests/WhenTheUserEntersAnInitialGrid.java
@@ -14,6 +14,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.openqa.selenium.WebDriver;
 
+
 @RunWith(ThucydidesRunner.class)
 @WithTag("Starting a new simulation")
 public class WhenTheUserEntersAnInitialGrid {
